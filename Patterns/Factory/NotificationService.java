5f806b5e777946151aeb3b30a1ff79820ea6e9e4
Mehedi Hassan Rico <mhrico2@gmail.com>
2022-11-17 14:28:14 +0600

    Complete NotificationService to run app

--- Patterns/Factory/NotificationService.java
+++ Patterns/Factory/NotificationService.java
@@ -1,7 +1,14 @@
 public class NotificationService {
     public static void main(String[] args) {
         NotificationFactory notificationFactory = new NotificationFactory();
-        Notification notification = notificationFactory.createNotification("push");
-        notification.notifyUser();
+        Notification pushNotification = notificationFactory.createNotification("push");
+        Notification smsNotification = notificationFactory.createNotification("sms");
+        Notification emailNotification = notificationFactory.createNotification("email");
+
+        String[] notifications = {"Notification 1", "Notification 2"};
+
+        pushNotification.notifyUser(notifications);
+        smsNotification.notifyUser(notifications);
+        emailNotification.notifyUser(notifications);
     }
 }
