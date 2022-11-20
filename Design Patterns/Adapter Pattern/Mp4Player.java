f9f5e1d88e199b39bfd67577eb033c8264f2ca9f
Mehedi Hassan Rico <mhrico2@gmail.com>
2022-11-20 18:24:08 +0600

    Implement Adapter Pattern

---
+++ Design Patterns/Adapter Pattern/Mp4Player.java
@@ -0,0 +1,9 @@
+public class Mp4Player implements AdvancedMediaPlayer{
+    public void playVlc(String fileName){
+    
+    }
+    
+    public void playMp4(String fileName){
+        System.out.println("Playing MP4 file " + fileName);
+    }
+}
