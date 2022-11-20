c52d8d3a06eeeacc4aae8afd4a1013e2d57b4363
Mehedi Hassan Rico <mhrico2@gmail.com>
2022-11-20 16:47:37 +0600

    Implement Facade Pattern

---
+++ Design Patterns/Facade Pattern/ShapeMaker.java
@@ -0,0 +1,23 @@
+public class ShapeMaker {
+    private Shape circle;
+    private Shape square;
+    private Shape rectangle;
+
+    public ShapeMaker(){
+        this.circle = new Circle();
+        this.square = new Square();
+        this.rectangle = new Rectangle();
+    }
+
+    public void drawCircle(){
+        circle.draw();
+    }
+
+    public void drawSquare(){
+        square.draw();
+    }
+
+    public void drawRectangle(){
+        rectangle.draw();
+    }
+}
