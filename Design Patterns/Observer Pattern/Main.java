516aab5d7b07b68fd5c974fa8bd0092be1aa79d1
Mehedi Hassan Rico <mhrico2@gmail.com>
2022-11-23 20:12:04 +0600

    Update Observer pattern

--- Design Patterns/Observer Pattern/Main.java
+++ Design Patterns/Observer Pattern/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Subject subject = new Subject();
+        new BinaryObserver(subject);
 
-        Observer binaryObserver = new BinaryObserver(subject);
-        System.out.println("15");
+        System.out.println("New state: 15");
         subject.setState(15);
+        System.out.println("New state: 20");
+        subject.setState(20);
+
     }
 }
